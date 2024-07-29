5db50cc73a0c9815571553697f4728404b446e43
pyclan88 <gumabemc@yandex.ru>
2024-07-29 11:20:35 +0300

    спринт 16 - исправлено отсутствие истории при нажатии на крестик или если стереть запрос с помощью клавиатуры - исправлены недочеты

--- app/src/main/java/com/practicum/playlistmaker/search/ui/activity/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/search/ui/activity/SearchActivity.kt
@@ -67,8 +67,7 @@ class SearchActivity : AppCompatActivity() {
 
         binding.clearIcon.setOnClickListener {
             binding.searchEditText.setText(R.string.empty_string)
-            adapter.tracks.clear()
-            adapter.notifyDataSetChanged()
+            showHistory()
         }
 
         binding.clearHistory.setOnClickListener {
@@ -92,7 +91,9 @@ class SearchActivity : AppCompatActivity() {
                 latestSearchText = s?.toString() ?: ""
             }
 
-            override fun afterTextChanged(s: Editable?) {}
+            override fun afterTextChanged(s: Editable?) {
+                if (s.isNullOrEmpty()) showHistory()
+            }
         }
 
         simpleTextWatcher.let { binding.searchEditText.addTextChangedListener(simpleTextWatcher) }
